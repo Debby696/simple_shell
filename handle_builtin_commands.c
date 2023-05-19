25c8636803b95d6fa52d615c4ec7a6dc51024f0e
Debby696 <idowuopeyemi696@gmail.com>
2023-05-19 04:41:06 -0700

    handle_builtin_commands

---
+++ handle_builtin_commands.c
@@ -0,0 +1,126 @@
+#include "shell.h"
+
+/**
+ * check_built_cmd - function that checks for built in commands
+ * @str: command line argument.
+ *  Return: 1 if a builtin argument is found 0 if none is found
+ */
+int check_built_cmd(char *str)
+{
+	if (str == NULL)
+		return (0);
+	if (_strcmp(str, "setenv") == 0)
+		return (1);
+	if (_strcmp(str, "unsetenv") == 0)
+		return (1);
+	if (_strcmp(str, "exit") == 0)
+		return (1);
+	if (_strcmp(str, "cd") == 0)
+		return (1);
+	return (0);
+}
+/**
+ * string_to_int - function that converts a string to an int.
+ * @str: string to be converted
+ * Return: str as an int.
+ */
+{
+	int num = 0; i = 0;
+	for (; str[i] !== '\0'; i++)
+	{
+		num = num * 10 + (str[i] - 48);
+	}
+	return (num);
+}
+/**
+ * check_digit - function that checks if all character in astring are digits
+ * @str: string input.
+ * Reteurn: 0 if all are string else -1.
+ */
+int check_digit(char *str)
+{
+	int x = 0;
+	while (str[x] != '\0')
+	{
+		if (str[x] >= '0' && str[x] <= '9')
+		{
+			x++;
+			continue;
+		}
+		return (-1);
+	}
+	return (0);
+}
+/**
+ * handle_exit - function that handle the exit built in command.
+ * @argv: list of command line strings
+ * @parent_name: name of calling process.
+ * Return: void.
+ */
+void handle_exit(char **argv, char *parent_name)
+{
+	char *err_msg;
+	int x = 0;
+
+	err_msg = _strcat(parent_name, ": ");
+	err_msg = _strcat(err_msg, argv[0]);
+
+	while (argv[x])
+	{
+		x++;
+	}
+	if (x == 1)
+	{
+		write(STDERR_FILENO, "exit\n", _str_len("exit\n"));
+		exit(0);
+	}
+	else if (x == 2 && check_digit(argv[1]) == 0)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDERR_FILENO, "exit\n", _str_len("exit\n"));
+		exit(string_to_int(argv[1]));
+	}
+	else if (x > 2 && check_digit(argv[1]) == 0)
+	{
+		err_msg = _strcat(parent_name, ": ");
+		err_msg = _strcat(err_msg, "exit: too many arguments\n");
+		write(STDERR_FILENO, err_msg, _str_len(err_msg));
+		return;
+	}
+	else
+	{
+		err_msg = _strcat(parent_name, ": ");
+		err_msg = _strcat(err_msg, "exit: ");
+		err_msg = _strcat(err_msg, argv[1]);
+		err_msg = _strcat(err_msg, ": numeric argument required\n");
+		write(STDERR_FILENO, err_msg, _str_len(err_msg));
+		exit(2);
+	}
+}
+/**
+ * handle_built_in_commands - function that handles shell built in commands.
+ * @argv: list of all arguments
+ * @env: list of environment variables.
+ * @parent_name: name of calling process.
+ * Return: void.
+ */
+void handle_built_in_commands(char **argv, char **env, char *parent_name)
+{
+	if (_strcmp(argv[0], "exit") == 0)
+	{
+		handle_exit(argv, parent_name);
+		return;
+	}
+	if (_strcmp(argv[0], "setenv") == 0)
+	{
+		handle_setenv(argv, env, parent_name);
+	}
+	if (_strcmp(argv[0], "unsetenv") == 0)
+	{
+		handle_unsetenv(argv, env, parent_name);
+	}
+	if (_strcmp(argv[0], "cd") == 0)
+	{
+		handle_cd(argv, env, parent_name);
+	}
+}
