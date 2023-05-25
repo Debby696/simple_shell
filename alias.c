62bfa3b95369e6352f38d95b424ae335ceff7ee8
Innocent_Bern <innocentbern22@gmail.com>
2023-05-25 04:16:20 +0300

    Added quote marks task 13

--- alias.c
+++ alias.c
@@ -36,6 +36,30 @@ void print_alias(char arr[4096][1000])
 	}
 }
 /**
+ * add_quotation - function that handles alias built in commands.
+ * @dest: dest string.
+ * @str: source string.
+ * Return: void.
+ */
+void add_quotation(char *dest, char *str)
+{
+	int x = 0;
+
+	while (*str != '\0')
+	{
+		if (*str == '\'')
+		{
+			x++;
+			str++;
+			continue;
+		}
+		*dest++ = *str++;
+		if (x == 2)
+			break;
+	}
+	_strcpy(&dest[_strlen(dest)], "'");
+}
+/**
  * handle_alias - function that handles alias built in commands.
  * @argv: list of cli arguments.
  * @parent: name of calling process.
@@ -48,33 +72,34 @@ void handle_alias(char **argv, char *parent)
 
 	while (argv[x] != NULL)
 		x++;
-
 	if (x == 1)
 		print_alias(arr);
-	len = 0;
-	x = 0;
+	len = x = 0;
 	while (argv[len] != NULL)
 	{
 		index = 0;
 		while (argv[len][x] != '\0' && argv[len][x] != '=')
 			x++;
-
 		if (argv[len][x] == '=')
 		{
 			_strncpy(&name[0], argv[len], x);
 			_strncpy(&cpy[0], arr[index], x);
-
+			if (argv[len][x + 1] != '\'')
 			while (arr[index][0] != '\0')
 			{
 				if (_strcmp(name, cpy) == 0)
 				{
-					_strcpy(&arr[index][0], argv[len]);
+					_strcpy(&name[_strlen(name)], "='");
+					add_quotation(&name[_strlen(name)], &argv[len][x + 1]);
+					_strcpy(&arr[index][0], name);
 					return;
 				}
 				_strncpy(&cpy[0], arr[index], x);
 				index++;
 			}
-			_strcpy(&arr[index][0], argv[len]);
+			_strcpy(&name[_strlen(name)], "='");
+			add_quotation(&name[_strlen(name)], &argv[len][x + 1]);
+			_strcpy(&arr[index][0], name);
 			return;
 		}
 		if (len >= 1)
